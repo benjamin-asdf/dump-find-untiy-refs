b55f005276f1fc5e0e3be9e423de9c118da0709e
Benj <Benjamin.Schwerdtner@gmail.com>
2019-07-26 15:56:07 +0200

    Improve overall, more stable, better output

--- dump_find_refs.nim
+++ dump_find_refs.nim
@@ -1,40 +1,63 @@
 import osproc, os, strformat, strutils
 
+# TODO This could be provived as an arg
 var unityProjectRoot: string = "./"
 
-proc assetPath(): string =
-  result = &"{unityProjectRoot}/Assets"
+var debug: bool = false
 
-proc outIfErr( msg: string, errC: int) =
+proc debugLog(s: string) =
+  if debug: echo s
+
+proc assetPath(): string =
+  result = &"{unityProjectRoot}Assets"
+
+proc outIfErr(output: string, errC: int) =
   if errC != 0:
-    echo &"exited with code {errC}:\n{msg}"
+    echo &"error code: {errC}\n output:{output}"
     quit(0)
+
+proc trimNewLine(s: var string) =
+  s.removeSuffix('\n')
+
 
 proc getUnityGuid(file: string): string =
   let awkcmd = """awk '{print $2}'"""
   let cmd = &"rg --no-ignore guid: {file}.meta | {awkcmd}"
+  debugLog(&"get unity guid... cmd is:\n{cmd}")
   var (guid, errc) = execCmdEx(cmd)
   outIfErr(guid,errc)
-  guid.removeSuffix('\n')
+  guid.trimNewLine()
   result = guid
 
+proc getDefinitionFiles(s: string): TaintedString =
+  let findFilesCmd = &"rg -l --no-ignore {s} {assetPath()}"
+  debugLog(&"get definition files... cmd is:\n{findFilesCmd}")
+  var (files, errC) = execCmdEx(findFilesCmd)
+  outIfErr(files,errC)
+  files.trimNewLine()
+  result = files
+
+
+if not existsDir(assetPath()):
+  echo "Enter a unity project root first."
+  quit(0)
 
 if paramCount() == 0:
   echo "Please provide the name of the type you want to search for."
-  quit(0);
+  quit(0)
 
-let findFilesCmd = &"rg -l --no-ignore {paramStr(1)} ./"
-let (files, errC) = execCmdEx(findFilesCmd)
+if paramCount() > 1 and paramStr(2) == "-d":
+  debug = true
 
-outIfErr(files,errC)
+let files = getDefinitionFiles(paramStr(1))
 
 for file in splitLines(files):
   if file != "":
     let guid = getUnityGuid(file)
-    if guid.contains('\n'):
-      echo "guid contains \'\\n\'", guid
-    else:
-      let cmd = &"rg -l --no-ignore -e \'guid: {guid}\' {unityProjectRoot}"
-      let (usages, errC) = execCmdEx(cmd)
-      outIfErr(usages, errC)
-      echo "file: ", file, " guid: ", guid, " usages:\n", usages
+    let cmd = &"rg -l --no-ignore -e \'guid: {guid}\' {assetPath()}"
+    let (usages, errC) = execCmdEx(cmd)
+    outIfErr(usages, errC)
+    for usagePath in splitLines(usages):
+      if usagePath != "":
+        echo &"{splitfile(file).name} {usagePath}"
+  else: echo "Warning! - handled an empty line, should not happen."
