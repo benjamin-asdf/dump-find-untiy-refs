3e5218cfd749b5cecc6c976c8012de6ad2027033
ben <benjamin.schwerdtner@gmail.com>
2020-02-20 07:32:50 +0100

    Add experimantal hacks

--- dump_find_refs.nim
+++ dump_find_refs.nim
@@ -9,7 +9,7 @@ proc debugLog(s: string) =
   if debug: echo s
 
 proc assetPath(): string =
-  result = &"{unityProjectRoot}/Assets"
+  result = unityProjectRoot / "Assets"
 
 proc outIfErr(output: string, errC: int) =
   if errC != 0:
@@ -19,26 +19,40 @@ proc outIfErr(output: string, errC: int) =
 proc trimNewLine(s: var string) =
   s.removeSuffix('\n')
 
+proc sanitizePath(path: string ): string =
+  result = replace(path,"\\","/")
+
+# proc cmdExists(c: string): bool =
+#   try:
+#     discard cmdExec(c)
+#     result = true
+#   except OSError:
+#     result = false
 
 proc getUnityGuid(file: string): string =
   # TODO I guess this usages of awk is retarded and we would split the string ourselves
-  let awkcmd = """awk '{print $2}'"""
-  let cmd = &"rg --no-ignore guid: {file}.meta | {awkcmd}"
+  # var (file, err) = execCmdEx(&"sed 's|\\\")
+  let cmd = &"rg --no-ignore guid: {sanitizePath(file)}.meta"
   debugLog &"get unity guid... cmd is:\n{cmd}"
-  var (guid, errc) = execCmdEx(cmd)
-  outIfErr(guid,errc)
+
+  var (rgGuidOutput, errc) = execCmdEx(cmd)
+  outIfErr(rgGuidOutput,errc)
+  var guid = rgGuidOutput.split(' ')[0]
   guid.trimNewLine()
   result = guid
 
+
 proc getDefinitionFiles(s: string): TaintedString =
-  let findFilesCmd = &"global -d {s} {assetPath()}"
-  debugLog &"get definition files... cmd is:\n{findFilesCmd}"
-  var (files, errC) = execCmdEx(findFilesCmd)
-  if files == "" or errC != 0:
-    let fallBackCmd = &"rg -l --no-ignore -e \"class\\s+{s}\\s+:\" {assetPath()}"
-    (files, errC) = execCmdEx(fallBackCmd)
-    # rg no match
-    if errC == 1: return ""
+
+  # let findFilesCmd = &"global -d {s} {assetPath()}"
+  # debugLog &"get definition files... cmd is:\n{findFilesCmd}"
+  # var (files, errC) = execCmdEx(findFilesCmd)
+  # if files == "" or errC != 0:
+
+  let fallBackCmd = &"rg -l --no-ignore -e \"class\\s+{s}\\s+:\" {assetPath()}"
+  var (files, errC) = execCmdEx(fallBackCmd)
+  # rg no match
+  if errC == 1: return ""
   outIfErr(files,errC)
   files.trimNewLine()
   result = files
@@ -60,7 +74,7 @@ for file in splitLines(getDefinitionFiles(typeQuery)):
   if file != "":
     let guid = getUnityGuid(file)
     # we put the ',' here after the guid enum, because we don't output meta files that way
-    let cmd = &"rg -l --no-ignore -e \'guid: {guid},\' {assetPath()}"
+    let cmd = &"rg -l --no-ignore -e \'guid: {guid},\' Assets/"
     debugLog &"try get guids, cmd: {cmd}"
     let (usages, errC) = execCmdEx(cmd)
     if (errC != 0): debugLog "error while getting guids"
